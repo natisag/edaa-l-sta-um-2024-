ea48afa52196fabfc7593be72de99549729d99ae
Nátali <129980148+natisag@users.noreply.github.com>
2024-02-25 17:43:09 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,36 @@
+class Animal {
+    private int id;
+    private String tipo;
+    private String habitat;
+
+    public Animal(int id, String tipo, String habitat) {
+        this.id = id;
+        this.tipo = tipo;
+        this.habitat = habitat;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+}
---
+++ Cachorro.java
@@ -0,0 +1,38 @@
+class Cachorro extends Animal {
+    private String nome;
+    private String raca;
+    private int idade;
+
+    public Cachorro(int id, String tipo, String habitat, String nome, String raca, int idade) {
+        super(id, tipo, habitat);
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+
+    // Getters e Setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
